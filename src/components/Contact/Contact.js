57f8789e0b6765b214816446e16114eda37b3d33
FilipCiesielski <filip.carpenter@gmail.com>
2019-12-13 12:13:29 +0100

    validation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,76 @@ import './Contact.scss';
 
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+
+       this.state = {
+            nameInput: "",
+            surnameInput: "",
+            emailInput: "",
+            messageInput: "",
+            checkBox: false,
+            formSend: false,
+            errName: false,
+            errSurname: false,
+            errEmail: false,
+
+        };
+        this.myRef = React.createRef();
+
+    }
+
+    handleOnChange = e => {
+        this.setState({
+            [e.target.name]: e.target.value })
+        console.log(this.myRef.current.style.color="red")
+
+
+    };
+
+    handleChangeBox = e => {
+        this.setState({checkBox: !this.state.checkBox});
+
+
+    };
+
+    handleOnSubmit = e => {
+        this.setState({
+            formSend: false, errName: false, errSurname: false, errEmail: false, errMessage: false
+        });
+
+        const mailReg = /^[0-9a-z_.-]+@[0-9a-z.-]+\.[a-z]{2,3}$/i;
+
+        let {nameInput, surnameInput, emailInput, checkBox,formSend} = this.state;
+
+
+        e.preventDefault();
+        if(nameInput!==""&& nameInput.length>1 && nameInput!==""&& nameInput.length>1 && mailReg.test(emailInput) && checkBox  ) {
+            this.setState({formSend: true})
+        }
+
+
+// const url = "https://fer-api.coderslab.pl/v1/portfolio/contact";
+//
+// fetch(url, {
+//     method: 'POST',
+//     headers: {
+//         "Content-Type": "application/json"
+//     },
+//     body: JSON.stringify({
+//         name, email, message
+//     })
+// })
+//     .then(function (data) {
+//         console.log(data);
+//     })
+//     .catch(function (error) {
+//     });
+
+    }
+
     render() {
+
         return (
             <div className={"contact__wrap"}>
 
@@ -11,32 +80,37 @@ class Contact extends Component {
                     <div className={"contact__box__form"}>
                         <span>Nie znalazłeś odpowiedniego kontaktu?</span>
                         <span>Skorzystaj z formularza.</span>
-                        <form>
+                        <form onSubmit={this.handleOnSubmit}>
                             <div className={"form__inputs"}>
                                 <div>
-                                    <input type={"name"} required=" "/>
+                                    <input type="name" name="nameInput" ref={this.myRef} required=" " value={this.state.nameInput}
+                                           onChange={this.handleOnChange}/>
                                     <label>Imię</label>
                                 </div>
                                 <div>
-                                    <input type={"name"} required=" "/>
+                                    <input type="name" name="surnameInput"  required=" "  value={this.state.surnameInput}
+                                           onChange={this.handleOnChange}/>
                                     <label>Nazwisko</label>
                                 </div>
                             </div>
                             <div className={"form__inputs"}>
                                 <div>
-                                    <input type={"email"} required=" "/>
+                                    <input type="email"  name="emailInput" required=" " value={this.state.emailInput}
+                                           onChange={this.handleOnChange}/>
                                     <label>Adres e-mail</label>
                                 </div>
 
                                 <div>
-                                    <textarea required={" "}/>
+                                    <textarea name="messageInput" required=" "  maxLength="2000" value={this.state.messageInput}
+                                              onChange={this.handleOnChange}/>
                                     <label>Treść Wiadomośći</label>
                                 </div>
                             </div>
 
                             <div className={"contact__box__checkBox"}>
                                 <div className="custom__checkbox">
-                                    <input type="checkbox" value="None" id="custom__checkbox" name="check"/>
+                                    <input type="checkbox" name="check" value="None" id="custom__checkbox"
+                                           onChange={this.handleChangeBox}/>
 
                                     <label htmlFor="custom__checkbox"></label>
                                 </div>
