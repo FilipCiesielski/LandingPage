94edb89783b256ac5ff2b1e70534a54baacf526a
FilipCiesielski <filip.carpenter@gmail.com>
2019-12-17 15:47:53 +0100

    Updates

---
+++ precache-manifest.411dc03b5f59165b3f4277167052e26a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509e6c1cba39854d202429dffcb920ce",
+    "url": "/LandingPage/index.html"
+  },
+  {
+    "revision": "6c5e979c1950b6b17d0e",
+    "url": "/LandingPage/static/css/main.f3cc6dff.chunk.css"
+  },
+  {
+    "revision": "3ca62e5341449f7170d4",
+    "url": "/LandingPage/static/js/2.df344b05.chunk.js"
+  },
+  {
+    "revision": "21fc6f2bfb761b7c470f0faec58fd5e4",
+    "url": "/LandingPage/static/js/2.df344b05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6c5e979c1950b6b17d0e",
+    "url": "/LandingPage/static/js/main.188ef9d4.chunk.js"
+  },
+  {
+    "revision": "a9685a0f2a1f355f2dc9",
+    "url": "/LandingPage/static/js/runtime-main.90afcc67.js"
+  },
+  {
+    "revision": "cdc71390997a42d82ab6dd93ff2f1f33",
+    "url": "/LandingPage/static/media/logo__eagle.cdc71390.png"
+  }
+]);
